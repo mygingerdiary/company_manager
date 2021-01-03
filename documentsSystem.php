9814e5d7257f6379b444fd93a502556cdad41911
doma <domciakostecka@gmail.com>
2021-01-03 17:14:06 +0100

    search box and validation

--- documentsSystem.php
+++ documentsSystem.php
@@ -10,6 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <title>Company Manager</title>
     <link href="style_inside.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         table, td, th, tr {
             border: 1px solid black;
@@ -19,12 +20,19 @@ session_start();
             margin-top: auto;
             text-align: center;
         }
+
     </style>
 
 </head>
 <body>
 <h1>Dokumenty</h1>
-<button onclick="window.location.href='addDocument.php'">Dodaj dokument</button>
+<button id="addDoc" onclick="window.location.href='addDocument.php'">Dodaj dokument</button>
+<form class="search" method="post" action="documentsSystem.php">
+    <input type="text" name="napis" placeholder="Wyszukaj...">
+    <button type="submit" name="submit"><i class="fa fa-search"></i></button>
+
+</form>
+
 <table style="width:1100px">
     <tr>
         <th>Id</th>
@@ -36,18 +44,50 @@ session_start();
     <?php
     $db = mysqli_connect("localhost", "root", "", "company_manager2");
 
-    $_SESSION['rzad']=array();
-    $result = "SELECT * FROM dokumenty";
-    $result=mysqli_query($db, $result);
+    if(!isset($_POST['submit'])) {
+        $_SESSION['rzad'] = array();
+        $result = "SELECT * FROM dokumenty";
+        $result = mysqli_query($db, $result);
 
-    if ($result->num_rows > 0) {
-        while ($data = $result->fetch_assoc()) {
-            array_push($_SESSION['rzad'],$data['id']);
-                echo "<tr><th>" . $data['id'] . "</th><th>" . $data['data'] . "</th><th>" . $data['l_stron'] . "</th><th>" . $data['notatki'] . "</th><th>".'<form method="post" action="showScan.php"><input type="submit" name="'.$data['id'].'" id=value="otwórz skan"></form>'."</td></tr>";
+        if ($result->num_rows > 0) {
+            while ($data = $result->fetch_assoc()) {
+                array_push($_SESSION['rzad'], $data['id']);
+                echo "<tr><th>" . $data['id'] . "</th><th>" . date('d-m-Y', strtotime($data['data'])) . "</th><th>" . $data['l_stron'] . "</th><th>" . $data['notatki'] . "</th><th>" . '<form method="post" action="showScan.php"><input type="submit" name="' . $data['id'] . '" value="otwórz skan"></form>'. "</th><th>" . '<button type="submit" name="delete" value="usuń">' . "</td></tr>";
             }
+        }
     }
 
+
+    if (isset($_POST['submit'])) {
+        $napis = $db->real_escape_string($_POST['napis']);
+        //echo intval($napis);
+        $idd = intval($napis);
+        if (is_int($idd) && $idd>0) {
+            $sql = "SELECT * FROM dokumenty WHERE id=$idd";
+            mysqli_query($db, $sql);
+            $result2 = mysqli_query($db, $sql);
+            if ($result2->num_rows > 0) {
+                while ($row = $result2->fetch_assoc()) {
+                    echo "<tr><th>" . $row['id'] . "</th><th>" . date('d-m-Y', strtotime($row['data'])) . "</th><th>" . $row['l_stron'] . "</th><th>" . $row['notatki'] . "</th><th>" . '<form method="post" action="showScan.php"><input type="submit" name="' . $row['id'] . '" value="otwórz skan"></form>' . "</td></tr>";
+                }
+            } else {
+                echo "</table>";
+                echo "Brak wyszukiwań";
+            }
+        } else {
+            $_SESSION['e_search'] = "Wyszukiwanie możliwe tylko po id";
+        }
+    }
     ?>
+    <?php
+    if(isset($_SESSION['e_search']))
+    {
+        echo"</table>";
+        echo '<div class="error2">'.$_SESSION['e_search'].'</div>';
+        unset($_SESSION['e_search']);
+    }
+    ?>
+
 </table>
 
 
